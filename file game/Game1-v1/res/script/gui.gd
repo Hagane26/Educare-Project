3726aa55fcba8bfa987d3a1013e36d8da4273972
senhermawan <senher346@gmail.com>
2024-10-03 17:46:07 +0700

    addd gui and updated old one

--- file game/Game1-v1/res/script/gui.gd
+++ file game/Game1-v1/res/script/gui.gd
@@ -3,21 +3,27 @@ extends Control
 @onready var timer = $Timer
 @onready var tx = $Panel/Label
 @onready var tx2 = $Panel/Label2
-
+var boxes = {}
+@export var text = ""
+@export var box_count = 1
 @export var sec = 0
 @export var min = 0
 var box = 0
 
 func _ready() -> void:
+	for i in range(box_count):
+		boxes[i] = "_______"
+	print(boxes)
 	timer.start(1)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	tx2.text = "Jumlah Box : " + str(box)
+	tx2.text = text.format(boxes)
 	pass
 
 func _on_timer_timeout() -> void:
+	#TODO : PAS NANTI TIMERNYA DI PAUSE PAS MUNCU MENU PAUSE
 	sec -= 1
 	if sec == 0:
 		min -= 1
@@ -27,6 +33,6 @@ func _on_timer_timeout() -> void:
 		timer.stop
 	pass # Replace with function body.
 
-func _on_target_box_correct() -> void:
-	box += 1
+func _on_target_box_correct(text,number) -> void:
+	boxes[number] = text
 	pass # Replace with function body.
--- file game/Game1-v1/res/script/target.gd
+++ file game/Game1-v1/res/script/target.gd
@@ -1,12 +1,12 @@
 extends Area2D
-
+@export var id = 0
 @export var target = ""
-signal box_correct
+signal box_correct(text,number)
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("box"):
 		if body.value == target:
 			body.can_move = false
-			emit_signal('box_correct')
+			box_correct.emit(target,id)
 		else:
 			print("no accept")
 	pass 
